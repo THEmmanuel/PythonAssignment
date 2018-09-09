6b216fedf1107c2cb673be2ebf60c1d38f72a1f8
Emmanuel Ayodele <emmayodayo@gmail.com>
2018-09-09 20:12:21 +0100

    Initial commit

---
+++ NumberEleven.py
@@ -0,0 +1,6 @@
+def swap(a, b):
+    a, b = b, a
+    return a, b
+
+
+print(swap(input("Input first number: "), input("Input second number: ")))
---
+++ NumberFour.py
@@ -0,0 +1,21 @@
+count = 0
+total = 0
+
+# Continue asking the user for input
+while True:
+    # Asks the user to input an integer
+    number = int(input("Please input a number: "))
+    total += number
+
+    # Breaks the operation when the user inputs 0
+    if number == 0:
+        print("Input was ZERO")
+        break
+    count = count + 1
+
+# Calculates the average of all given integers
+average = total / count
+print("the average is ", average)
+
+# Returns the sum of all integers
+print("The sum of all numbers is ", total)
